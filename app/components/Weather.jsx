fcc9d9dfc99aa166d70936e0a0c62119d6f46e9e
Akshay Arora <akshay575@yahoo.com>
2016-08-20 21:01:42 +0530

    Initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
+
+var Weather = React.createClass({
+  getInitialState: function(){
+    return {
+      isLoading: false
+    }
+  },
+  handleSearch: function(location){
+    var self = this;
+    
+    // Before Search Returns Value
+    this.setState({isLoading: true});
+
+    openWeatherMap.getTemp(location).then(function(temp){
+      self.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+    }, function(err){
+      self.setState({isLoading: false});
+      alert(err);
+    })
+  },
+  render: function(){
+    var {isLoading, location, temp} = this.state;
+
+    function renderMessage(){
+      if(isLoading){
+        return <h3>Fetching current weather...</h3>
+      }
+      else if(location && temp){
+        return <WeatherMessage location={location} temp={temp}/>;
+      }
+    }
+    return (
+      <div>
+        <h3>Weather Component</h3>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+      </div>
+    );
+  }
+});
+
+module.exports = Weather;
