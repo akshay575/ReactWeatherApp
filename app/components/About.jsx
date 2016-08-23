b1136cf2d47d0b1b6e1a46543463b16bf29becc2
Akshay Arora <akshay575@yahoo.com>
2016-08-23 21:42:35 +0530

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,15 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function(){
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
-
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a realtime weather application build on ReactJS. Here are some of the tools used to build this app:</p>
+      <ul>
+        <li><a href="">ReactJS</a> - This is the Javascript Framework used.</li>
+        <li><a href="">Open Weather Map</a> - It uses Open Weather Map API to retrieve current weather data by city name.</li>
+      </ul>
+    </div>
   );
 }
 
